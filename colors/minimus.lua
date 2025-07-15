e97dc6b7993ad070c1d7c4d4c04e279b9edd8ade
scabbage <thescabbage@gmail.com>
2025-07-16 05:30:19 +0800

    Add `minimus` theme

---
+++ colors/minimus.lua
@@ -0,0 +1,69 @@
+--vim.highlight.priorities.semantic_tokens = 95
+vim.o.background = "dark"
+--vim.cmd("syntax off");
+
+local type = "#86ea99";
+local func = "#a989dc";
+local look = "#eb1a51";
+local cons = "#95caee";
+local text = "#c4ced6";
+local hide = "#2d2a32";
+
+vim.api.nvim_set_hl(0, "Normal", { fg = "#ffffff", bg = "#080008" })
+vim.api.nvim_set_hl(0, "Cursor", { fg = "#000000", bg = "#9e79fc" })
+vim.cmd([[set guicursor=n-v-c:block,i-ci-ve:ver25,
+        \r-n:hor100,cr:hor16,o:hor50,a:blinkwait1000-blinkoff5000-blinkon1-Cursor,
+        ]]);
+
+-- standouts
+vim.api.nvim_set_hl(0, "Comment", { fg = look, italic = true })
+vim.api.nvim_set_hl(0, "@comment", { fg = look, italic = true })
+vim.api.nvim_set_hl(0, "@spell", { fg = look })
+vim.api.nvim_set_hl(0, "@lsp.type.xmlDocCommentText", { fg = look })
+
+vim.api.nvim_set_hl(0, "Error", { fg = look, bold = true })
+vim.api.nvim_set_hl(0, "Warning", { fg = look, bold = true })
+
+-- hidden
+vim.api.nvim_set_hl(0, "Whitespace", { fg = hide })
+
+-- typical
+vim.api.nvim_set_hl(0, "Identifier", { fg = text })
+vim.api.nvim_set_hl(0, "Variable", { fg = text })
+vim.api.nvim_set_hl(0, "@variable", { fg = text })
+vim.api.nvim_set_hl(0, "Text", { fg = text })
+vim.api.nvim_set_hl(0, "Delimiter", { fg = text })
+vim.api.nvim_set_hl(0, "@delimiter", { fg = text })
+vim.api.nvim_set_hl(0, "@lsp.type.xmlDocCommentDelimiter", { fg = look })
+vim.api.nvim_set_hl(0, "Punctuation", { fg = text })
+vim.api.nvim_set_hl(0, "@punctuation", { fg = text })
+vim.api.nvim_set_hl(0, "@lsp.type.punctuation", { fg = text })
+vim.api.nvim_set_hl(0, "@lsp.type.field", { fg = text })
+vim.api.nvim_set_hl(0, "@spell.gitcommit", { fg = text })
+
+-- func/mutation
+vim.api.nvim_set_hl(0, "Function", { fg = func, italic = true })
+vim.api.nvim_set_hl(0, "@function", { fg = func, italic = true })
+vim.api.nvim_set_hl(0, "Operator", { fg = func, })
+vim.api.nvim_set_hl(0, "@operator", { fg = func, })
+
+-- constants
+vim.api.nvim_set_hl(0, "String", { fg = cons })
+vim.api.nvim_set_hl(0, "@string", { fg = cons })
+vim.api.nvim_set_hl(0, "Constant", { fg = cons })
+vim.api.nvim_set_hl(0, "@constant", { fg = cons })
+vim.api.nvim_set_hl(0, "@keyword", { fg = cons })
+vim.api.nvim_set_hl(0, "@lsp.type.keyword", { fg = cons })
+vim.api.nvim_set_hl(0, "@lsp.type.controlKeyword", { fg = cons })
+vim.api.nvim_set_hl(0, "@keyword.operator", { fg = cons })
+
+-- types
+vim.api.nvim_set_hl(0, "@type", { fg = type })
+vim.api.nvim_set_hl(0, "Type", { fg = type })
+vim.api.nvim_set_hl(0, "@attribute", { fg = type })
+vim.api.nvim_set_hl(0, "@lsp.type", { fg = type })
+vim.api.nvim_set_hl(0, "@type.builtin", { fg = type })
+
+-- Oil.nvim
+vim.api.nvim_set_hl(0, "OilDir", { fg = cons })
+vim.api.nvim_set_hl(0, "OilFile", { fg = text })
